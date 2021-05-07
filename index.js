2f7de4c41d6db5c965f1bf7c5ec0c11ee699d807
Khadisha97 <khadisha.dabayeva@gmail.comgit config --global user.name Github khadishadabayeva>
2021-05-07 11:50:55 +0200

    sending message from a server

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const ws = require('ws')
 const cors = require('cors')
-
+const SocketIO = require('socket.io')
 const corsOptions = {
     origin: 'http://localhost:8080'
 }
@@ -11,12 +10,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const wsServer = new ws.Server({ noServer: true });
-
-app.use((req, res, next) => {
-    res.wss = wsServer
-    next()
-})
+// const wsServer = new ws.Server({ noServer: true });
 
 app.get('/', (req, res) => {
     res.json({
@@ -26,18 +20,33 @@ app.get('/', (req, res) => {
 
 const http = require('http').createServer(app)
 
+const io = SocketIO(http, {
+    cors: {}
+})
+
+io.on('connection', (socket) => {
+    console.log('Got a connection')
+
+    socket.on('message', (arg) => {
+        console.log('got a connection', arg)
+        socket.on('message', (arg) => {
+            console.log('Got a message:', arg)
+            setTimeout(() => {
+                socket.emit('serverMessage', 'Hello from server')
+            }, 2000)
+        })
+    })
+})
+
 const PORT = process.env.PORT || 8081
 
-wsServer.on('connection', socket => {
-    socket.on('message', message => console.log(message));
-})
 
 const server = http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`)
 })
 
-server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, socket => {
-        wsServer.emit('connection', socket, request)
-    })
-})
+// server.on('upgrade', (request, socket, head) => {
+//     wsServer.handleUpgrade(request, socket, head, socket => {
+//         wsServer.emit('connection', socket, request)
+//     })
+// })
